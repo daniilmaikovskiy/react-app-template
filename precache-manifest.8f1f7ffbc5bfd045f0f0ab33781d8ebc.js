8ecda90476993a5d70684bbab52b843b44d57601
daniilmaikovskiy <maikovsky.danil@gmail.com>
2020-07-23 21:09:46 +0300

    Updates

---
+++ precache-manifest.8f1f7ffbc5bfd045f0f0ab33781d8ebc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adf3919b8fafcaf46e98b5d79a993737",
+    "url": "/react-app-template/index.html"
+  },
+  {
+    "revision": "73b1e7e82e466b3d6b5b",
+    "url": "/react-app-template/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "dac96ca1b7b0a1f80c08",
+    "url": "/react-app-template/static/css/main.edb7f315.chunk.css"
+  },
+  {
+    "revision": "73b1e7e82e466b3d6b5b",
+    "url": "/react-app-template/static/js/2.70ac93b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app-template/static/js/2.70ac93b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dac96ca1b7b0a1f80c08",
+    "url": "/react-app-template/static/js/main.6b98ead4.chunk.js"
+  },
+  {
+    "revision": "2efb07d73b82e0ec74cc",
+    "url": "/react-app-template/static/js/runtime-main.e461483d.js"
+  }
+]);
